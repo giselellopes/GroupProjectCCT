b43a548c7f81b9fbe1381baa27b3ef636b5ac156
sarahkuprian <sarahcarrinho@gmail.com>
2022-05-20 22:17:16 +0000

    commit

---
+++ my-express-server/public/index.js
@@ -0,0 +1,91 @@
+const   express = require("express"),
+        app = express(),
+        bodyParser = require("body-parser"),
+        mongoose = require("mongoose");
+        
+        
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://admin:ATGP11db@cluster0.hkmhc.mongodb.net/atgpdb")
+
+
+
+const customerSchema = new mongoose.Schema({
+    email: {type: String, unique: true, lowercase: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+    flag: {
+        type: String, 
+        enum: ['visa', 'mastercard', 'elo']
+    },
+    lastFourDIgits: Number,
+    cardHolder: String,
+    address: String,
+    phone: Number 
+
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
+
+
+
+const developerSchema = new mongoose.Schema({
+    username: {type: String, unique: true, lowercase: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+    bank: String,
+    iban: String,
+    city: String,
+    state: String,
+    zip: String
+
+});
+
+
+module.exports = mongoose.model('Developer', developerSchema);
+
+
+
+
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/register.handlebars");
+})
+
+app.post("/", function(req, res) {
+    let newDeveloper = new Developer({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: req.body.username,
+        password: req.body.password,
+        bank: req.body.bank,
+        iban: req.body.iban,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip
+    })
+    newDeveloper.save();
+})
+
+/*let app = express();
+let port = process.env.PORT || 3000;
+
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(logger("tiny"));
+app.use(require('./routes'));
+
+
+mongoose.connect(process.env.atgpdbURI, { useNewUrlParser: true,
+useUnifiedTopology: true })
+        .then((result) => console.log('connected to atgpdb'))
+        .catch((err) => console.log(err));*/
+
+app.listen(3000, function(){
+    console.log("Server is working");
+})
