3e0c21eaed04f3a90f447c3b1890724fafd1a8a6
CarlosGoncalves89 <carlos.h.goncalves@outlook.com>
2022-07-27 19:34:47 -0300

    Auth

--- my-express-server/routes/index.js
+++ my-express-server/routes/index.js
@@ -1,12 +1,19 @@
 const { Router } = require('express');
 const router = Router();
 
+const LocalStorage = require('node-localstorage').LocalStorage;
+
+const ensureAuthenticated = require('../public/utils/EnsureAuthenticated');
+
 //routes for login
 router.get("/", function (req, res) {
     res.render('index');
 });
 
 router.get("/login", function (req, res) {
+    const localStorage = new LocalStorage('./scratch');
+    localStorage._deleteLocation();
+
     res.render('login');
 });
 
@@ -18,8 +25,8 @@ router.get("/create-project", function (req, res) {
     res.render('create-project');
 });
 
-router.get("/proposal", function (req, res) {
-    res.render('proposal');
+router.get("/proposal", ensureAuthenticated, function (req, res) {
+    es.render('proposal')
 });
 
 module.exports = router
